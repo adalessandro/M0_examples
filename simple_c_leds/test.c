4c1a1911d145781fd3ed2ef04abaafb02615735e
Ariel D'Alessandro <ariel.dalessandro@gmail.com>
2015-04-06 11:38:10 -0300

    simple_c_leds: define macros are better!
    Signed-off-by: Ariel D'Alessandro <ariel@vanguardiasur.com.ar>

--- simple_c_leds/test.c
+++ simple_c_leds/test.c
@@ -1,17 +1,19 @@
-void delay(void) {
+#define GPIO_ON_P7	(int*) 0x400f629c
+#define GPIO_OFF_P7	(int*) 0x400f631c
+#define REDLED		0x20
+#define DELAY		0xFFFFF
+
+void sleep(void) {
 	unsigned int i;
-	for(i = 0; i < 0xFFFFF; i++);
+	for(i = 0; i < DELAY; i++);
 }
 
 int c_entry(void) {
-	unsigned int *gpio_on_p7 = (int*) 0x400f629c; //gpio port on 7 register
-	unsigned int *gpio_off_p7 = (int*) 0x400f631c; //gpio port off 7 register
-	unsigned int redled = 0x20;
 	while(1) {
-		delay();
-		*gpio_on_p7 = redled;
-		delay();
-		*gpio_off_p7 = redled;
+		sleep();
+		*(GPIO_ON_P7) = REDLED;
+		sleep();
+		*(GPIO_OFF_P7) = REDLED;
 	}
 	return 0;
 }
